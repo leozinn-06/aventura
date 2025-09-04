5f425447f5408fcec23ac4fdd3e21bc25e172328
leozinn-06 <leonardo.moura.calixto@escola.pr.gov.br>
2025-09-04 09:07:14 -0300

    ok

---
+++ script.js/script.js
@@ -0,0 +1,15 @@
+const avanca = document.querySelectorAll('.btn-proximo);
+    const reiniciarBtn = ducument.getElementById('.btn-reiniciar);
+
+avanca.forEach(butto => {
+butto.addEventListener('click', fuction(){
+    const atual = document.querySelector('ativo');
+    const proximoPasso= 'passo-' +this.getAtteibute('data-proximo')
+})
+
+
+}
+
+
+
+)
